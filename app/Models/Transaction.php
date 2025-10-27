cc1b139b05df25a4966dc173e2858618756e996e
HablSank <hablsankrumipradhab@gmail.com>
2025-10-27 11:19:16 +0700

    DB Seeder and Faker

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+
+    public function transactiondetail(){
+        return $this->hasMany(TransactionDetail::class);
+    }
+        
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
